796f2a99cd91d85e4616b2ed13d9b6bfca7e74c4
Abdellahsyani <syaniabdos@gmail.com>
2024-08-11 21:01:48 +0100

    glam rock

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- writing a sql script that lists all bands
-
+-- band_name using IFNULL and FIND_IN_SET
 SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
 	FROM metal_bands
 	WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0

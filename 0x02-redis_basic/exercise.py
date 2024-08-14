8df9fd8818b2a97a0127be791e695f5706dd6fe3
Abdellahsyani <syaniabdos@gmail.com>
2024-08-14 17:06:29 +0100

    update the cach class

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 '''Create a Cache class'''
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -12,7 +12,7 @@ class Cache:
         '''The init method'''
 
         self._redis = redis.Redis()
-        self._redis.flushdb(True)
+        self._redis.flushdb()
 
     def store(self, data: Union[str, int, bytes, float]) -> str:
         '''Store method to return the key value as a string'''
@@ -20,3 +20,18 @@ class Cache:
         key_data = str(uuid.uuid4())
         self._redis.set(key_data, data)
         return key_data
+
+    def get(self, key_data: str, fn: Optional[Callable] = None) ->
+    Union[str, int, bytes, float, None]:
+        '''retrieve the data from cache by using the callable'''
+        key = self._redis.get(key_data)
+        if key is not None and fn is not None:
+            return fn(key)
+        return key
+
+    def get_str(self, key_data: str) -> Optional(str):
+        '''retreive data as a string'''
+        return self.get(key_data, lambda d: d.decode('utf-8'))
+
+    def get_int(self, key_data: str) -> Optional(int):
+        return self.get(key_data, int)

d48aca4de68320359ca60a9e514d0ce26227a4e7
Abdellahsyani <syaniabdos@gmail.com>
2024-08-14 17:12:30 +0100

    try to fix

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -21,17 +21,18 @@ class Cache:
         self._redis.set(key_data, data)
         return key_data
 
-    def get(self, key_data: str, fn: Optional[Callable] = None) ->
-    Union[str, int, bytes, float, None]:
+    def get(self,
+            key_data: str,
+            fn: Callable = None) -> Union[str, int, bytes, float]:
         '''retrieve the data from cache by using the callable'''
         key = self._redis.get(key_data)
         if key is not None and fn is not None:
             return fn(key)
         return key
 
-    def get_str(self, key_data: str) -> Optional(str):
+    def get_str(self, key_data: str) -> str:
         '''retreive data as a string'''
         return self.get(key_data, lambda d: d.decode('utf-8'))
 
-    def get_int(self, key_data: str) -> Optional(int):
+    def get_int(self, key_data: str) -> int:
         return self.get(key_data, int)

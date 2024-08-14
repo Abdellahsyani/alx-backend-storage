8a19ae907ee5d363c0d7408a7e987ca3ceabfa8a
Abdellahsyani <syaniabdos@gmail.com>
2024-08-14 12:20:51 +0100

    writing string to redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -12,7 +12,7 @@ class Cache:
         self._redis = redis.Redis(host='localhost', port=6379, db=0)
         self._redis.flushdb()
 
-    def store(self, data: Union[str, int, bytes, float]) -> str:
+    def store(self, data: Union[str, int, bytes, float]) -> Union[str, None, None, None]:
         '''Store method to return the key value as a string'''
         key = str(uuid.uuid4())
         self._redis.set(key, data)

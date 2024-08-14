77eba3ee9fb10143cf652ad2ba12a5723f354c07
Abdellahsyani <syaniabdos@gmail.com>
2024-08-14 12:48:20 +0100

    writing string to redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -7,13 +7,16 @@ from typing import Union
 
 class Cache:
     '''Start caching with Redis'''
+
     def __init__(self) -> None:
         '''The init method'''
+
         self._redis = redis.Redis()
         self._redis.flushdb(True)
 
-    def store(self, data: Union[str, int, bytes, float]) -> Union[str, None, None, None]:
+    def store(self, data: Union[str, int, bytes, float]) -> str:
         '''Store method to return the key value as a string'''
+
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
